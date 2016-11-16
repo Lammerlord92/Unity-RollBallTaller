689484adc6fcb4c2e3106bffad7762b397ae7f6b
Lammerlord92 <lammerlord92@gmail.com>
2016-11-16 14:41:34 +0100

    Terminado el proyecto

---
+++ Assets/Scripts/PlatformScript.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlatformScript : MonoBehaviour {
+	public float force=5f;
+	void OnTriggerStay(Collider _other){
+		Vector3 forceUp=Vector3.up * force;
+		if (_other.gameObject.tag == "Player") {
+			_other.attachedRigidbody.velocity=forceUp;
+		}
+	}
+}
