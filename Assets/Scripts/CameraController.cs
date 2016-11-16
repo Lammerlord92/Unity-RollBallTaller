a99633214b475681c1b37ac411bc1cb9e5a65534
Lammerlord92 <lammerlord92@gmail.com>
2016-11-16 12:41:59 +0100

    Añadido jugador y cambio de cámara

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+	private Vector3 offset;
+	public Transform playerTransform;
+		
+	void Start(){
+		offset =transform.position-playerTransform.position ;
+	}
+	// Update is called once per frame
+	void Update () {
+		transform.position = playerTransform.position+offset;
+		//transform.LookAt (playerTransform);
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+	private Rigidbody myRigidBody;
+	public float speed;
+	// Use this for initialization
+	void Start () {
+		myRigidBody = GetComponent<Rigidbody> ();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		float moveHorizontal = Input.GetAxisRaw ("Horizontal");
+		float moveVertical = Input.GetAxisRaw ("Vertical");
+		Vector3 force = new Vector3 (moveHorizontal, 0, moveVertical);
+		myRigidBody.AddForce (force*speed);
+
+	}
+}
