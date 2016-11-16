2a813d77ea3e15237fb9492767fc9b56a5d3ccb1
Lammerlord92 <lammerlord92@gmail.com>
2016-11-16 13:42:41 +0100

    Sigue movimiento de ratón, clicks del ratón al desconectar y terminada la escena.

--- Assets/Scripts/PlayerControllerMouse.cs
+++ Assets/Scripts/PlayerControllerMouse.cs
@@ -2,10 +2,11 @@
 using System.Collections;
 using UnityEngine.UI;
 
-public class PlayerController : MonoBehaviour {
+public class PlayerControllerMouse : MonoBehaviour {
 	//Movimiento del jugador
 	public float speed;
 	private Rigidbody myRigidBody;
+	private Vector3 nextPosition;
 	//Objetos coleccionables
 	public Text text;
 	private float collectionableObjects;
@@ -18,11 +19,20 @@ public class PlayerController : MonoBehaviour {
 		text.text="Collectionable objects: 0";
 	}
 	//Movimiento del jugador
+	void Update(){
+		//if (Input.GetMouseButtonDown (0)) {
+			Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+			RaycastHit hit;
+			if (Physics.Raycast (ray, out hit)) {
+				//myRigidBody.velocity = Vector3.zero;
+				nextPosition = hit.point;
+			}
+		//}
+	}
 	void FixedUpdate () {
-		float moveHorizontal = Input.GetAxisRaw ("Horizontal");
-		float moveVertical = Input.GetAxisRaw ("Vertical");
-		Vector3 force = new Vector3 (moveHorizontal, 0, moveVertical);
-		myRigidBody.AddForce (force*speed);
+		Vector3 direction =nextPosition-transform.position;
+		direction.Normalize ();
+		myRigidBody.AddForce (direction*speed);
 
 	}
 	//Objetos coleccionables
